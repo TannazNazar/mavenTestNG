04bc7bfc5cd6dd1bef74c415a270a1089d717b88
Tannaz <Tanaz.nazar@gmail.com>
2018-12-29 12:09:53 -0500

    last update

--- src/test/java/com/syntax/orangeHRM/test/initClass.java
+++ src/test/java/com/syntax/orangeHRM/test/initClass.java
@@ -3,6 +3,11 @@ package com.syntax.orangeHRM.test;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 import org.testng.AssertJUnit;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -17,16 +22,22 @@ public class initClass {
 	@BeforeTest
 	public void setUp() {
 		driver= new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(10,TimeUnit.SECONDS);
 		
 		
 	}
 	
 	
 	@Test
-	public void testRun() {
+	public void testRun() throws InterruptedException {
 		driver.get("http://google.com");
 		String title=driver.getTitle();
 		AssertJUnit.assertEquals(title, "Google");
+		driver.findElement(By.name("q")).sendKeys("restaurant in fairfax");
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+//		Thread.sleep(10000);
+//		driver.findElement(By.name("btnK")).click();
 	}
 	
 	@AfterTest
